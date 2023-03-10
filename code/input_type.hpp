8032f42f2435b38c23a2ffa68969a0238603533e
Mario Linares <mariolinares@csu.fullerton.edu>
2023-03-10 14:04:13 -0800

    Added comments and spacing between functions

--- code/input_type.hpp
+++ code/input_type.hpp
@@ -9,6 +9,7 @@
 #include <sstream>
 #include "procedure_functions.h"
 
+// Functions used to determine input type
 bool isSeparator(char input);
 bool isOperator(char input);
 bool isUnderline(char input);
@@ -16,6 +17,7 @@ bool isDot(char input);
 bool isKeyword(std::string input);
 std::string operator_or_separator(char input);
 bool isWhitespace(char input);
+
 //2FSMs and helper functions
 int FSM_col_determiner(char input);
 int idFSM(int state, char input);
@@ -23,6 +25,6 @@ bool is_accepting_id(int state);
 int intrealFSM(int state, char input);
 bool is_accepting_intreal(int state);
 
-//determine token type based on final state------------------------------------------
+//Determine token type based on final state
 std::string ending_state_intreal(int state);
 std::string ending_state_id(int state);
